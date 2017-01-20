0ca9bd8b79d4a1049c0c90b057cffc47bd3f3b03
sandhya008 <sandhyagarg008@gmail.com>
2017-01-19 23:30:22 -0800

    intial commit

---
+++ src/test/java/githubtrail/LoginTest.java
@@ -0,0 +1,35 @@
+package githubtrail;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setUp(){
+		
+		driver = new ChromeDriver();
+		
+	}
+	@Test
+	public void doLogin(){
+		
+		driver.get("http://google.com");
+		driver.findElement(By.xpath("//*[@id='lst-ib']")).sendKeys("news");
+		////*[@id="lst-ib"]
+		
+		//driver.findElement(By.xpath(".//*[@id='_fZl']")).click();
+	}
+	@AfterSuite
+	public void tearDown(){
+		
+		driver.quit();
+	}
+
+}
